e3896cf0d106c9a8fec208bd5a150f50968af7be
Ichiro <suzukiichiro05@gmail.com>
2018-10-26 17:57:32 -0700

    Got a lot Done

--- Assets/Walking.cs
+++ Assets/Walking.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Walking : MonoBehaviour {
 
+	public float frontSpeed = 5f;
+	public float backSpeed = 5f;
+
+	public float moveHorizontal;
+	public float moveVertical;
+	public Rigidbody player;
+	private Vector3 frontmovement;
+	private Vector3 backmovement;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +19,30 @@ public class Walking : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		
+		
+		//if(Input.GetKey("w")) {
+		//	transform.Translate(Vector3.forward * frontSpeed * Time.deltaTime);
+		//}
+		//if(Input.GetKey("s")) {
+		//	transform.Translate(Vector3.forward * backSpeed * Time.deltaTime);
+		//}
+		
+		//moveVertical = Input.GetAxis("Vertical") * backSpeed;
+		//moveHorizontal = Input.GetAxis("Horizontal") * backSpeed;
+		//transform.Translate(moveHorizontal, moveVertical, 0);
+
+
+		frontmovement = new Vector3(Input.GetAxis("Horizontal") * frontSpeed, player.velocity.y, Input.GetAxis("Vertical") * frontSpeed);
+		backmovement = new Vector3(Input.GetAxis("Horizontal") * frontSpeed, player.velocity.y, Input.GetAxis("Vertical") * backSpeed);
+
+		if(Input.GetKey("w")) {
+			player.velocity = frontmovement;
+		}
+		if(Input.GetKey("s")) {
+			player.velocity = backmovement;
+		}
 		
 	}
 }
