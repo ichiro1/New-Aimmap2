88d9a6dea08941f37fe75aca91ed4a63c18e060c
UCode <ucode@UCODE-TPV-ETA.fios-router.home>
2018-11-02 18:02:22 -0700

    More Movement

--- Assets/Walking.cs
+++ Assets/Walking.cs
@@ -6,12 +6,21 @@ public class Walking : MonoBehaviour {
 
 	public float frontSpeed = 5f;
 	public float backSpeed = 5f;
+    public float leftSpeed = 5f;
+    public float rightSpeed = 5f;
 
 	public float moveHorizontal;
 	public float moveVertical;
 	public Rigidbody player;
 	private Vector3 frontmovement;
 	private Vector3 backmovement;
+    private Vector3 leftmovement;
+    private Vector3 rightmovement;
+    private Vector3 frontleftmovement;
+    private Vector3 frontrightmovement;
+    private Vector3 backleftmovement;
+    private Vector3 backrightmovement;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -20,29 +29,42 @@ public class Walking : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		
-		
-		//if(Input.GetKey("w")) {
-		//	transform.Translate(Vector3.forward * frontSpeed * Time.deltaTime);
-		//}
-		//if(Input.GetKey("s")) {
-		//	transform.Translate(Vector3.forward * backSpeed * Time.deltaTime);
-		//}
-		
-		//moveVertical = Input.GetAxis("Vertical") * backSpeed;
-		//moveHorizontal = Input.GetAxis("Horizontal") * backSpeed;
-		//transform.Translate(moveHorizontal, moveVertical, 0);
 
 
-		frontmovement = new Vector3(Input.GetAxis("Horizontal") * frontSpeed, player.velocity.y, Input.GetAxis("Vertical") * frontSpeed);
-		backmovement = new Vector3(Input.GetAxis("Horizontal") * frontSpeed, player.velocity.y, Input.GetAxis("Vertical") * backSpeed);
+        //if(Input.GetKey("w")) {
+        //	transform.Translate(Vector3.forward * frontSpeed * Time.deltaTime);
+        //}
+        //if(Input.GetKey("s")) {
+        //	transform.Translate(Vector3.forward * backSpeed * Time.deltaTime);
+        //}
 
-		if(Input.GetKey("w")) {
-			player.velocity = frontmovement;
+        //moveVertical = Input.GetAxis("Vertical") * backSpeed;
+        //moveHorizontal = Input.GetAxis("Horizontal") * backSpeed;
+        //transform.Translate(moveHorizontal, moveVertical, 0);
+
+
+        leftmovement = new Vector3(Input.GetAxis("Horizontal") * frontSpeed, player.velocity.y);
+        rightmovement = new Vector3(Input.GetAxis("Horizontal") * frontSpeed, player.velocity.y);
+        frontmovement = new Vector3(0.0f, player.velocity.x, leftSpeed);
+        backmovement = new Vector3(0.0f, player.velocity.x, rightSpeed);
+
+        frontleftmovement = new Vector3(Input.GetAxis("Horizontal") * frontSpeed, 0.0f , leftSpeed);
+
+        if(Input.GetKey("a")) {
+			player.velocity = leftmovement;
 		}
-		if(Input.GetKey("s")) {
-			player.velocity = backmovement;
+		if(Input.GetKey("d")) {
+			player.velocity = rightmovement;
 		}
-		
+
+        if(Input.GetKey("w")) {
+            player.velocity = frontmovement;
+        }
+        if(Input.GetKey("s")) {
+            player.velocity = backmovement;
+        }
+        if(Input.GetKey("a") && Input.GetKey("w")) {
+            player.velocity = frontleftmovement;
+        }
 	}
 }
