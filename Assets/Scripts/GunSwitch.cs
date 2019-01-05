c74901fd89b60ee68a620b4d2b6cfd01e25d8521
UCode <ucode@UCODE-TPV-NU.fios-router.home>
2019-01-04 17:56:15 -0800

    Gun Switching

--- Assets/Scripts/GunSwitch.cs
+++ Assets/Scripts/GunSwitch.cs
@@ -7,29 +7,64 @@ public class GunSwitch : MonoBehaviour {
 	public Transform gunbody2;
 	public Transform playerTransform;
 	public int gunbodytransform = 1;
-	
+	public Renderer Ak;
+	public Renderer Colt;
+	public Renderer AkMagazine;
+	public Renderer AkShell;
+	public Renderer ColtShell;
+	public Renderer ColtMagazine;
+	public Renderer ColtSlide;
+	private bool gunSwitcher;
+
 	// Use this for initialization
 	void Start () {
 		gunbody1.GetComponent<Transform>();
 		gunbody2.GetComponent<Transform>();
 		if(true) {
 		Debug.Log (gunbody1.position);
+			Ak.GetComponent<MeshRenderer> ();
+			Colt.GetComponent<MeshRenderer> ();
+			Colt.enabled = false;
+
+			AkMagazine.GetComponent<MeshRenderer> ();
+			AkShell.GetComponent<MeshRenderer> ();
+			ColtShell.GetComponent<MeshRenderer> ();
+			ColtMagazine.GetComponent<MeshRenderer> ();
+			ColtSlide.GetComponent<MeshRenderer> ();
+
+			ColtShell.enabled = false;
+			ColtMagazine.enabled = false;
+			ColtSlide.enabled = false;
 		}
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		/*if(Input.GetKeyDown("2")) {
-			gunbody1.position = new Vector3(10,10,10);
-			gunbody2.position = new Vector3(1,14,1);
-			gunbody1.GetComponent<MeshRenderer>().enabled =false;
+		
+		if (Input.GetKeyDown ("1")) {
+			gunSwitcher = true;
 		}
-		if(Input.GetKeyDown("1")) {
-			gunbody1.position = new Vector3(1,14,1);
-			gunbody2.position = new Vector3(10,10,10);
-			gunbody1.GetComponent<MeshRenderer>().enabled = true;
-			
-		}*/
+		if (Input.GetKeyDown ("2")) {
+			gunSwitcher = false;
+		}
+		if (gunSwitcher == false) {
+			Ak.enabled = true;
+			AkMagazine.enabled = true;
+			AkShell.enabled = true;
+
+			Colt.enabled = false;
+		} 
+		else if(gunSwitcher == true){
+			Colt.enabled = true;
+			ColtShell.enabled = true;
+			ColtMagazine.enabled = true;
+			ColtSlide.enabled = true;
+
+			Ak.enabled = false;
+			AkMagazine.enabled = true;
+			AkShell.enabled = true;
+		}
+
 	}
 }
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Shoot : MonoBehaviour {
 	public Animation anim;
 	public bool ifShooting;
+	public AudioSource aud;
+	private float frameShotCount = 9;
 	// Use this for initialization
 	void Start () {
 		ifShooting = false;
+		aud.GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
@@ -15,10 +18,18 @@ public class Shoot : MonoBehaviour {
 		if (Input.GetMouseButton (0)) {
 			ifShooting = true;
 			anim.Play ();
+			frameShotCount += 1;
 		}
 		if (Input.GetMouseButtonUp (0) && ifShooting == true) {
 			ifShooting = false;
 			anim.Stop ();
+			aud.Stop ();
+		}
+		if (frameShotCount == 10) {
+			aud.Play ();
+		}
+		if(frameShotCount == 15) {
+			frameShotCount = 1;
 		}
 	}
 }
