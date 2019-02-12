ee4d8c077f4c8445ebfc0eec1703d7fa80cf78ec
Tyler <rothtyler24@gmail.com>
2019-02-11 16:51:54 -0800

    Oh This is Weird

---
+++ Assets/Scripts/ScopeIn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeIn : MonoBehaviour {
+
+	float m_FieldOfView = 60f;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(Camera.main.fieldOfView);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(1)) {
+			Camera.main.fieldOfView = m_FieldOfView;
+		}
+	}
+}
