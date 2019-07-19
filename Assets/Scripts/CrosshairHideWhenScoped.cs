ba0f330a7a1882aa05323fcd02aaacf4ee5064e3
UCode <ucode@UCODE-TPV-MU.fios-router.home>
2019-07-19 16:32:17 -0700

    Animation Reworks

---
+++ Assets/Scripts/CrosshairHideWhenScoped.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairHideWhenScoped : MonoBehaviour
+{
+    public Image CrossHairParts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(1)) {
+            CrossHairParts.enabled = false;
+        }
+        else if(Input.GetMouseButtonUp(1)) {
+            CrossHairParts.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/MaxFrameRate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxFrameRate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
